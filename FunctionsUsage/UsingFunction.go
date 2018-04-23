81e762bc456489a70195e4746a9038cd191c61d9
Aashirwad Gupta <agupta@ces.local>
2018-04-23 17:05:10 +0530

    Restructuring the packages

---
+++ FunctionsUsage/UsingFunction.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Salutation struct {
+	name     string
+	greeting string
+}
+
+func Greet(sal Salutation) {
+	fmt.Println(sal.greeting + " " +sal.name)
+}
+
+func main() {
+	fmt.Println("Hello, playground")
+	var s = Salutation{"Aashirwad Gupta", "Hello!"}
+	Greet(s)
+}
