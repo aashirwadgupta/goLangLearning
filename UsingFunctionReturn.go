ae0df58d18bca9cba2a9545c2343633b1fa431cf
Aashirwad Gupta <aash.gupta2@gmail.com>
2018-04-23 15:25:26 +0530

    UsingFunctionReturn.go
    File to use another function in a function. 
    The function now returns a value which is then printed

---
+++ UsingFunctionReturn.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Salutation struct {
+	name     string
+	greeting string
+}
+
+func Greet(sal Salutation) {
+	fmt.Println(sal.greeting + " " +sal.name)
+	fmt.Println(CreateMessage(sal.greeting, sal.name))
+}
+
+
+func CreateMessage(param1, param2 string) string{
+    var joinedStr = param1 + " Joined with " +param2
+    return joinedStr
+}
+
+func main() {
+	fmt.Println("Hello, playground")
+	var s = Salutation{"Bob", "Hello!"}
+	Greet(s)
+}
